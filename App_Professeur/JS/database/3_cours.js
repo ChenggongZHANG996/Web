901eb4dd56650360ba0314f31773dcb36007e49e
chenggongZHANG <chenggongzhang@etu.estia.fr>
2025-01-26 23:33:41 +0100

    Prof_Autre_calendrier_obtenir les donnees

--- App_Professeur/JS/database/3_cours.js
+++ App_Professeur/JS/database/3_cours.js
@@ -1,3 +1,27 @@
 import { baseUrl } from "../../../Configuration_Js/base-url.js";
-import { dbService } from "../../../Configuration_Js/db-service.js";
 import { supabaseClient } from "../../../Configuration_Js/supabase-config.js";
+
+class CourseDB {
+  constructor() {
+    this.db = supabaseClient;
+  }
+
+  async getCourses() {
+    try {
+      const { data, error } = await this.db
+        .from('professor_courses')
+        .select('*')
+        .order('created_at', { ascending: false });
+
+      if (error) throw error;
+      return data;
+    } catch (error) {
+      console.error("Error in CourseDB.getCourses:", error);
+      throw error;
+    }
+  }
+
+  // 添加其他数据库操作方法
+}
+
+export const courseDB = new CourseDB();
--- App_Professeur/JS/services/3_cours.js
+++ App_Professeur/JS/services/3_cours.js
@@ -1,3 +1,22 @@
 import { baseUrl } from "../../../Configuration_Js/base-url.js";
 import { supabaseClient } from "../../../Configuration_Js/supabase-config.js";
-import { courseDB } from "../database/3_cours.js";  // 使用正确的数据库文件名
+import { courseDB } from "../database/3_cours.js";
+
+class CourseService {
+  constructor() {
+    this.db = courseDB;
+  }
+
+  async getCourses() {
+    try {
+      return await this.db.getCourses();
+    } catch (error) {
+      console.error("Error in CourseService.getCourses:", error);
+      throw error;
+    }
+  }
+
+  // 添加其他课程相关的服务方法
+}
+
+export const courseService = new CourseService();
